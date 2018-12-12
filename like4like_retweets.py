c5221d3eb3aa3e145dc0a4d0a342ab472d7347bf
anmolsinghal <anmolsinghal1998@gmail.com>
2018-12-12 18:31:50 +0530

    changes in scraper

--- like4like_retweets.py
+++ like4like_retweets.py
@@ -2,8 +2,6 @@ import requests
 import ConfigParser
 import re
 import datetime
-import contextlib
-from bs4 import BeautifulSoup
 
 setting_file="config/config.txt"
 configs=ConfigParser.ConfigParser()
@@ -17,7 +15,7 @@ payload = {
 
 print("****** LIKE4LIKE PARSER ******")
 
-fw =  open('likes/likes_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.csv','a+')
+fw =  open('retweets/retweets_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.csv','a+')
 
 print('Using Credentials:' + str(payload))
 
@@ -25,13 +23,3 @@ Session=requests.session()
 r=Session.post('https://www.like4like.org/api/login.php',data=payload)
 
 print(r)
-
-tweetCounter=0
-
-likesPageContent = Session.get("https://www.like4like.org/user/earn-twitter-favorites.php").content
-
-tweetPoints=re.findall('<span id="value-credits.*</span>',likesPageContent)
-
-for x in tweetPoints:
-	print(x)
-
